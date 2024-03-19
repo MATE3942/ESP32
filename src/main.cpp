48d072e1a0ecf29ebd0594326b72b1372f521501
Mateo <mateo.avalos3942@gmail.com>
2024-03-18 22:13:19 -0500

    Added project LEDWebPAge

--- src/main.cpp
+++ src/main.cpp
@@ -1,147 +1,112 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// Function Declarations
-void updateSerial();
-void test_sim800_module();
-void send_SMS();
-void recieve_SMS();
+const char *WIFI_NAME = "";
+const char *WIFI_PASSWORD = "";
 
-void setup() {
+WiFiServer server(80);
+
+String header;
+
+String LED_STATE = "off";
+
+const int GPIOpin = 23;
+
+void setup()
+{
     Serial.begin(115200);
-    Serial2.begin(115200);
-    delay(3000);
-    Serial.print("\nWant to test SIM800 Module? (y/n): ");
-    while(!Serial.available()){}
-    char test = Serial.read();
-    if (test == 'y') {
-        test_sim800_module();
+    pinMode(GPIOpin, OUTPUT);
+
+    digitalWrite(GPIOpin, LOW);
+
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_NAME);
+    WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+    Serial.print("Trying to connect to Wifi Network");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+       Serial.print(".");
     }
-    else if (test == 'n') {
-        Serial.println("Skipping SIM800 Module Test");
-    } else {
-        Serial.println("Invalid Input");
-    }
+    Serial.println("");
+    Serial.println("Successfully connected to WiFi network");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
 }
 
-void loop() {
-    Serial.println("\nWhat do you want to do?");
-    Serial.println("1. Check connection status");
-    Serial.println("2. Recieve Location");
-    Serial.println("3. Check Signal Quality");
-    Serial.println("4. Check Operator Information");
-    Serial.println("5. Exit");
-    Serial.print("Enter your choice: ");
-    while(!Serial.available()){}
-    int choice = Serial.parseInt();
-    switch (choice) {
-        case 1:
-            Serial.println("Checking Connection Status");
-            Serial2.println("AT");
-            updateSerial();
-            Serial.println();
-            break;
-        case 2:
-            Serial.println("Recieving Location");
-            //Serial2.println("AT+CIPGSMLOC=?"); // Test GPS Module
-            Serial2.println("AT+CIPGSMLOC=1,1"); // Get Location, time and date
-            updateSerial();
-            Serial.println();
-            break;
-        case 3:
-            Serial.println("Checking Signal Quality");
-            Serial2.println("AT+CSQ");
-            updateSerial();
-            Serial.println();
-            break;
-        case 4:
-            Serial.println("Checking Operator Information");
-            Serial2.println("AT+COPS?");
-            updateSerial();
-            Serial.println();
-            break;
-        case 5:
-            Serial.println("Exiting");
-            exit(0);
-            break;
-        default:
-            Serial.println("Invalid Choice");
-            break;
-    }
-    Serial.println();
-}
-
-// Function Definitions
-void updateSerial() {
-    delay(500);
-    while (Serial.available()) {
-        Serial2.write(Serial.read()); // Forward what Serial received to Software Serial Port
-    }
-    while (Serial2.available()) {
-        Serial.write(Serial2.read()); // Forward what Software Serial received to Serial Port
-    }
-}
-
-void test_sim800_module() {
-    Serial.println("Testing SIM800 Module\n");
-
-    Serial.println("Communication with the module");
-    Serial2.println("AT");
-    updateSerial();
-    Serial.println("-----------------------------");
-
-    Serial.println("Signal Quality: should be more than 5");
-    Serial2.println("AT+CSQ");
-    updateSerial();
-    Serial.println("-----------------------------");
-
-    Serial.println("SIM card ID");
-    Serial2.println("AT+CCID");
-    updateSerial();
-    Serial.println("-----------------------------");
-
-    Serial.println("Network Registration: 1 for home network and 5 for public network");
-    Serial2.println("AT+CREG?");
-    updateSerial();
-    Serial.println("-----------------------------");
-
-    Serial.println("Product Information");
-    Serial2.println("ATI");
-    updateSerial();
-    Serial.println("-----------------------------");
-
-    Serial.println("Battery Information");
-    Serial2.println("AT+CBC");
-    updateSerial();
-    Serial.println("-----------------------------");
-
-    Serial.println("Operator Information");
-    Serial2.println("AT+COPS?");
-    updateSerial();
-    Serial.println("-----------------------------");
-}
-
-void send_SMS() {
-    Serial2.println("AT+CMGF=1"); // Configuring TEXT mode
-    updateSerial();
-    Serial2.println("AT+CMGS=\"+573226137339\""); // Phone number to SMS
-    updateSerial();
-    Serial2.print("Circuit Digest"); // text content
-    updateSerial();
-    Serial.println();
-    Serial.println("Message Sent");
-    Serial2.write(26);
-}
-
-void recieve_SMS() {
-    Serial2.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
-    updateSerial();
-    Serial.println("Waiting for SMS");
-    delay(10000);
-    if (Serial2.available() > 0) {
-        String msg = Serial2.readString();
-        Serial.println("Message Received: " + msg);
-    }
-    else {
-        Serial.println("No Message Received");
+void loop()
+{
+    WiFiClient client = server.available();
+    if (client)
+    {
+        Serial.println("New Client is requesting web page");
+        String current_data_line = "";
+        while (client.connected())
+        {
+            if (client.available())
+            {
+                char new_byte = client.read();
+                Serial.write(new_byte);
+                header += new_byte;
+                if (new_byte == '\n')
+                {
+                    if (current_data_line.length() == 0)
+                    {
+                        // Send a response to the client
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+                        // Check the value of the pin
+                        if (header.indexOf("LED1=ON") != -1)
+                        {
+                            Serial.println("GPIO23 LED is ON");
+                            LED_STATE = "on";
+                            digitalWrite(GPIOpin, HIGH);
+                        }
+                        if (header.indexOf("LED1=OFF") != -1)
+                        {
+                            Serial.println("GPIO23 LED is OFF");
+                            LED_STATE = "off";
+                            digitalWrite(GPIOpin, LOW);
+                        }
+                        // Display the HTML web page
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println("<link rel=\"icon\" href=\"data:,\">");
+                        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                        client.println(".button { background-color: #4CAF50; border: 2px solid #4CAF50;; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }");
+                        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                        // Web Page Heading
+                        client.println("</style></head>");
+                        client.println("<body><center><h1>ESP32 Web server LED control</h1></center>");
+                        client.println("<center><h2>Press Button to interact</h3></center>");
+                        client.println("<form><center>");
+                        client.println("<p> LED one is " + LED_STATE + "</p>");
+                        // If the PIN_NUMBER_22State is off, it displays the ON button
+                        client.println("<center> <button class=\"button\" name=\"LED1\" value=\"ON\" type=\"submit\">LED1 ON</button>");
+                        client.println("<button class=\"button\" name=\"LED1\" value=\"OFF\" type=\"submit\">LED1 OFF</button><br><br>");
+                        client.println("</center></form></body></html>");
+                        client.println();
+                        break;
+                    }
+                    else
+                    {
+                        current_data_line = "";
+                    }
+                }
+                else if (new_byte != '\r')
+                {
+                    current_data_line += new_byte;
+                }
+            }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
     }
 }

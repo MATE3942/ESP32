fbfbef688cb3d206b978961fc35847af030d7ac4
Mateo <mateo.avalos3942@gmail.com>
2024-03-18 21:19:16 -0500

    Added project PWM_LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,30 @@
 #include <Arduino.h>
 
+int led = 23; // the PWM pin the LED is attached to
+int brightness = 0; // how bright the LED is
+int fadeAmount = 5; // how many points to fade the LED by
+
 void setup()
 {
-  Serial.begin(115200);
-  // initialize LED digital pin as an output.
-  pinMode(GPIO_NUM_23, OUTPUT);
-  Serial.println("Pin 23 initialized for output");
+  Serial.begin(115200); // set up Serial library at 115200 bps
+  pinMode(led, OUTPUT); // initialize LED digital pin as an output.
+  Serial.println("Pin 23 initialized for PWM output.");
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(GPIO_NUM_23, HIGH);
-  // wait for a second
-  delay(1000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(GPIO_NUM_23, LOW);
-   // wait for a second
-  delay(1000);
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);
+ 
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+ 
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  Serial.print("The LED Brightness is at: ");
+  Serial.println(brightness);
+  // wait for 30 milliseconds to see the dimming effect
+  delay(50);
 }
